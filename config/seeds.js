9964a260af04b7677ea929e827227ff248ee2ea6
Sergio Medina <medinaommedina@gmail.com>
2021-05-16 14:40:09 -0700

    seed database with 2 users and each user has 6 cars

--- config/seeds.js
+++ config/seeds.js
@@ -30,84 +30,96 @@ const carsArr = [{
     model: "Model S",
     color: "Blue",
     year: 2021,
-    VIN: "tms9822"
+    VIN: "tms9822",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Tesla",
     model: "Model S",
     color: "Yellow",
     year: 2021,
-    VIN: "tms6710"
+    VIN: "tms6710",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Tesla",
     model: "Model X",
     color: "Orange",
     year: 2021,
-    VIN: "tmx9432"
+    VIN: "tmx9432",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Tesla",
     model: "Model SX",
     color: "Red",
     year: 2021,
-    VIN: "tmsx6718"
+    VIN: "tmsx6718",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Tesla",
     model: "Model 3",
     color: "Gray",
     year: 2021,
-    VIN: "tm36710"
+    VIN: "tm36710",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Tesla",
     model: "Model 3X",
     color: "Green",
     year: 2021,
-    VIN: "tm3x6382"
+    VIN: "tm3x6382",
+    owner: "60a185529293eed030af2166"
 }, {
     mileage: 0,
     make: "Lucid",
     model: "Lumen",
     color: "Purple",
     year: 2021,
-    VIN: "ll17673"
+    VIN: "ll17673",
+    owner: "60a185529293eed030af2167"
 }, {
     mileage: 0,
     make: "Lucid",
     model: "Lumen 2",
     color: "Pink",
     year: 2021,
-    VIN: "ll28903"
+    VIN: "ll28903",
+    owner: "60a185529293eed030af2167"
 }, {
     mileage: 0,
     make: "Photon",
     model: "Mc2",
     color: "White",
     year: 2021,
-    VIN: "pmc21972"
+    VIN: "pmc21972",
+    owner: "60a185529293eed030af2167"
 }, {
     mileage: 0,
     make: "Photon",
     model: "Mc3",
     color: "Lime",
     year: 2021,
-    VIN: "pmc36732"
+    VIN: "pmc36732",
+    owner: "60a185529293eed030af2167"
 }, {
     mileage: 0,
     make: "Cyber",
     model: "0ms",
     color: "Black",
     year: 2021,
-    VIN: "c01724"
+    VIN: "c01724",
+    owner: "60a185529293eed030af2167"
 }, {
     mileage: 0,
     make: "Cyber",
     model: "10ms",
     color: "Teal",
     year: 2021,
-    VIN: "c108975"
+    VIN: "c108975",
+    owner: "60a185529293eed030af2167"
 }
 ]
 
@@ -116,59 +128,120 @@ const usersArr = [
     {firstName: "Wonder", lastName: "Woman", money: 0, cars:[]}
 ]
 
-const seedDb = () => {
-    //drop cars table and make seeds
-    /* comment in to drop db
-     mongoose.connection.collections.cars.drop(function(){
-            console.log("cars db dropped")
+// const seedDb = () => {
+//     //drop cars table and make seeds
+//     /* comment in to drop db
+//      mongoose.connection.collections.cars.drop(function(){
+//             console.log("cars db dropped")
 
-            carsArr.forEach(carKeys => {
-                const newCar = new Car(carKeys)
-                newCar.save()
-                .then(console.log(`car: ${carKeys.make} ${carKeys.model} created`))
-            })
-            // console.log("making cars DONE")
-    })
-    */
+//             carsArr.forEach(carKeys => {
+//                 const newCar = new Car(carKeys)
+//                 newCar.save()
+//                 .then(console.log(`car: ${carKeys.make} ${carKeys.model} created`))
+//             })
+//             console.log("making cars DONE")
+//     })
+//     // */
 
-    mongoose.connection.collections.users.drop(function() {
-     const carsOwnedArray = [
-            "609f5f6e782666b11eefd377",
-            "609f5f6e782666b11eefd378",
-            "609f5f6e782666b11eefd379",
-            "609f5f6e782666b11eefd37a",
-            "609f5f6e782666b11eefd37b",
-            "609f5f6e782666b11eefd37c",
-            "609f5f6e782666b11eefd37d",
-            "609f5f6e782666b11eefd380",
-            "609f5f6e782666b11eefd37f",
-            "609f5f6e782666b11eefd37e",
-            "609f5f6e782666b11eefd381",
-            "609f5f6e782666b11eefd382"
-        ]
+//     // mongoose.connection.collections.users.drop(function() {
+//     // //  const carsOwnedArray = [
+//     // //         "609f5f6e782666b11eefd377",
+//     // //         "609f5f6e782666b11eefd378",
+//     // //         "609f5f6e782666b11eefd379",
+//     // //         "609f5f6e782666b11eefd37a",
+//     // //         "609f5f6e782666b11eefd37b",
+//     // //         "609f5f6e782666b11eefd37c",
+//     // //         "609f5f6e782666b11eefd37d",
+//     // //         "609f5f6e782666b11eefd380",
+//     // //         "609f5f6e782666b11eefd37f",
+//     // //         "609f5f6e782666b11eefd37e",
+//     // //         "609f5f6e782666b11eefd381",
+//     // //         "609f5f6e782666b11eefd382"
+//     // //     ]
         
-        //make users and attach IDs s Strings
+//     //     //make users and attach IDs s Strings
         
-        usersArr.forEach((userKeys, idx) => {
-            const startCarCountAt = [0,6];
-            let count = 0
-            let end = startCarCountAt[idx] + 6;
+//     //     // usersArr.forEach((userKeys, idx) => {
+//     //     //     const startCarCountAt = [0,6];
+//     //     //     let count = 0
+//     //     //     let end = startCarCountAt[idx] + 6;
 
-            console.log("making User")
-            const newUser = new User(userKeys)
+//     //     //     console.log("making User")
+//     //     //     const newUser = new User(userKeys)
 
-            for (let i = startCarCountAt[idx]; i < end; i++){
-                newUser.cars.push(carsOwnedArray[i])
-                console.log("add car")
-            }
+//     //     //     for (let i = startCarCountAt[idx]; i < end; i++){
+//     //     //         newUser.cars.push(carsOwnedArray[i])
+//     //     //         console.log("add car")
+//     //     //     }
 
 
-            newUser.save()
-            .then(console.log(`user: ${userKeys.firstName} ${newUser.cars} created`))
+//     //     //     newUser.save()
+//     //     //     .then(console.log(`user: ${userKeys.firstName} ${newUser.cars} created`))
             
-        })
-    })
+//     //     // })
+
+//     //     //make solo users without populating Car's array with ObjIDs
+//     //     usersArr.forEach((userKeys, idx) => {
+//     //         console.log("making User")
+//     //         const newUser = new User(userKeys)
+
+//     //         newUser.save()
+//     //         .then(console.log(`user: ${userKeys.firstName} ${newUser.cars} created`))
+        
+//     //     })
+//     // })
+// }
+const seedDb = async () => {
+    const carsOwnedArray = []
+    
+
+    const allCars = await Car.find({})
+    //allCars if Object
+    // allCarsKeys = Object.keys(allCars)
+    for(let id in allCars){
+        for(let key in allCars[id]){
+            if(key === "_id"){
+                // console.log(allCars[id][key])
+                carsOwnedArray.push(allCars[id][key])
+            }
+        }
+    }
+    let carsPart1 = carsOwnedArray.slice(0,6)
+    let carsPart2 = carsOwnedArray.slice(6,12)
+
+    console.log(carsPart1)
+    console.log(carsPart2)
+
+    // { $concat: { cars: carspart1 }},
+    // User.findOneAndUpdate(
+    //     { _id: "60a185529293eed030af2166" },
+    //     { "$push": { "cars": { "$each": carsPart1 }}},
+    //     {returnOriginal: false, useFindAndModify: false }
+    // ).then(user => user.save())
+
+    // { $concat: { cars: carspart2 }},
+    // User.findOneAndUpdate(
+    //     { _id: "60a185529293eed030af2167" },
+    //     { "$push": { "cars": { "$each": carsPart2 }}},
+    //     {returnOriginal: false, useFindAndModify: false }
+    // ).then((user)=> user.save())
+
 }
+
+// Friend.findOneAndUpdate(
+//    { _id: req.body.id }, 
+//    { $push: { friends: objFriends  } },
+//   function (error, success) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log(success);
+//         }
+//     });
+// )
+
+
+
 
 
 //  const newUser = new User(usersArr[0])
--- server/resolvers/queries/car/car.js
+++ server/resolvers/queries/car/car.js
@@ -1,5 +1,3 @@
 module.exports = async(_, args, {models}) => {
     return await models.Car.findById({_id: args.id})
 }
-
-/
